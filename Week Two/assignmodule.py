117de44de97451b329fcea8a462c3c108d9303ed
ADoroh2122 <139788035+ADoroh2122@users.noreply.github.com>
2023-09-20 18:33:32 -0500

    Create assignmodule.py

---
+++ Week Two/assignmodule.py
@@ -0,0 +1,18 @@
+
+# Create a Module in VS Code and import it into a Notebook. Use each of the functions you build in your module
+# Your module should have the following capabilities:
+
+# Has a function to calculate the square footage of a house
+
+def square_footage_house(l, w):
+    return l * w
+
+# Has a function to calculate the circumference of a circle
+
+def circumference_of_a_circle(radius):
+    return round(2 * 3.141592653 * radius, 2)
+
+# Has a function to change feet to inches
+
+def feet_to_inches(feet):
+    return feet * 12
